6e22f60609ec9798aa2afae9fc60510bda669356
Caderrik <caderrik@gmail.com>
2016-11-22 17:21:24 +0100

    1st working version

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,37 @@
 FROM ubuntu
 MAINTAINER Caderrik <caderrik@gmail.com>
 
+################################################################################
+## app infos
+ENV ADMINPASSWORD="changeit" INSTALL_DIR=/chivalry
+VOLUME ["/chivalry"]
+EXPOSE 7000 7010
+
+################################################################################
+## app deps
 RUN set -x && \
     dpkg --add-architecture i386 && \
     apt-get update -qq && \
     apt-get install -qq curl libstdc++6:i386 lib32gcc1
 
-RUN useradd -m -u 1000 steam
+################################################################################
+## cleaning as root
+RUN apt-get clean autoclean purge && \
+    rm -fr /tmp/*
 
+RUN useradd -r -m -u 1000 steam
+
+################################################################################
+## volume
+RUN mkdir -p "${INSTALL_DIR}" && \
+    chown steam -R "${INSTALL_DIR}" && \
+    chmod 755 -R "${INSTALL_DIR}"
+
+################################################################################
+## app config
 COPY run.sh /usr/local/bin/run-chivalry
 
-RUN mkdir -p /chivalry && \
-    chown -R steam:steam /chivalry && \
-    chmod +x /usr/local/bin/run-chivalry
-
+################################################################################
+## app run
 USER steam
-VOLUME ["/chivalry"]
 ENTRYPOINT ["/usr/local/bin/run-chivalry"]
--- run.sh
+++ run.sh
@@ -1,20 +1,36 @@
 #!/usr/bin/env bash
 
-if [ ! -d "/chivalry/steamcmd" ]; then
-    mkdir -p /chivalry/steamcmd
-    cd /chivalry/steamcmd
+function install-steamcmd {
+    echo "steamcmd not found. Installing..."
+    mkdir -p "${INSTALL_DIR}/steamcmd"
+    cd "${INSTALL_DIR}/steamcmd"
     curl -Ss http://media.steampowered.com/installer/steamcmd_linux.tar.gz | tar -xz
-fi
+}
 
-/chivalry/steamcmd/steamcmd.sh +login anonymous +force_install_dir /chivalry +app_update 220070 +quit
+function install-chivalry {
+    echo "No game files found. Installing..."
+    "${INSTALL_DIR}/steamcmd/steamcmd.sh" +login anonymous +force_install_dir "${INSTALL_DIR}/server" +app_update 220070 +quit
+    if [ ! -L "${INSTALL_DIR}/server/Binaries/Linux/lib/steamclient.so" ]; then
+        ln -s "${INSTALL_DIR}/server/steamclient.so" "${INSTALL_DIR}/server/Binaries/Linux/lib/steamclient.so"
+    fi
 
-if [ ! -L "/chivalry/Binaries/Linux/lib/steamclient.so" ]; then
-    ln -s /chivalry/steamclient.so /chivalry/Binaries/Linux/lib/steamclient.so
-fi
+    if [ ! -f "${INSTALL_DIR}/server/Binaries/Linux/steam_appid.txt" ]; then
+        echo "219640" > "${INSTALL_DIR}/server/Binaries/Linux/steam_appid.txt"
+    fi
+}
 
-if [ ! -f "/chivalry/Binaries/Linux/steam_appid.txt" ]; then
-    echo "219640" > "/chivalry/Binaries/Linux/steam_appid.txt"
-fi
+##################################### Main #####################################
+# we check if steamcmd is installed
+[ ! -f "${INSTALL_DIR}/steamcmd" ] && install-steamcmd
 
-export LD_LIBRARY_PATH=/chivalry/linux64:/chivalry/Binaries/Linux/lib
-/chivalry/Binaries/Linux/UDKGameServer-Linux
+# we check if chivalry is installed or need an update
+install-chivalry
+
+[ ! -d "${INSTALL_DIR}/config" ] && mkdir -p "${INSTALL_DIR}/config"
+
+cd "${INSTALL_DIR}/config"
+[ ! -L PCServer-UDKGame.ini ] && ln -s ../server/UDKGame/Config/PCServer-UDKGame.ini PCServer-UDKGame.ini
+
+export LD_LIBRARY_PATH=${INSTALL_DIR}/server/linux64:${INSTALL_DIR}/server/Binaries/Linux/lib
+cd "${INSTALL_DIR}/server/Binaries/Linux"
+./UDKGameServer-Linux aocffa-moor_p\?steamsockets\?adminpassword=${ADMINPASSWORD}\?port=7000\?queryport=7010 -seekfreeloadingserver
